1ad093f448fab206c80fc30ce7c336b51ac7aaf8
cleytonRR <cleytonrodrigues69@live.com>
2021-01-03 00:32:33 -0300

    Refactor queries

--- landing-page/src/graphql/queries/getLandingPage.ts
+++ landing-page/src/graphql/queries/getLandingPage.ts
@@ -1,12 +1,31 @@
 import { gql } from 'graphql-request'
 
 const GET_LANDING_PAGE = gql`
+  fragment logo on LandingPage {
+    logo {
+      url
+      alternativeText
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      description
+      image {
+        alternativeText
+        url
+      }
+      button {
+        label
+        url
+      }
+      title
+    }
+  }
   query GET_LANDING_PAGE {
     landingPage {
-      logo {
-        url
-        alternativeText
-      }
+      ...logo
+      ...header
     }
   }
 `
