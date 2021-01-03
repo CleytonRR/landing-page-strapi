c40febf97ddc6c6861bfee53a459d0271e2ca73b
cleytonRR <cleytonrodrigues69@live.com>
2021-01-03 00:32:08 -0300

    Create types to header

--- landing-page/src/types/api.ts
+++ landing-page/src/types/api.ts
@@ -3,6 +3,20 @@ export type LogoProps = {
   url: string
 }
 
+export type HeaderProps = {
+  description: string
+  image: {
+    alternativeText: string
+    url: string
+  }
+  button: {
+    label: string
+    url: string
+  }
+  title: string
+}
+
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
 }
